0415f3b151836ba363922f6fc5ab61f2e9b985aa
gioboa <giorgiob.boa@gmail.com>
2019-12-02 01:02:03 +0100

    feat: simple state implementations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = "dashboard-view";
+export class AppComponent implements OnChanges {
+  @Input()
+  state = {};
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (!!changes['state']) {
+      this.state = JSON.parse(changes['state'].currentValue);
+      console.log(this.state);
+    }
+  }
 }
