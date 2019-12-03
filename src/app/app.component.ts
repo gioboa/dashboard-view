5252eca6d5357b8e914cd526132da388ecee115b
gioboa <giorgiob.boa@gmail.com>
2019-12-03 23:49:31 +0100

    feat: added chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +10,43 @@ export class AppComponent implements OnChanges, OnInit {
   @Input()
   state: any = {};
 
+  colors = [
+    {
+      fillColor: 'rgba(47, 132, 71, 0)',
+      strokeColor: 'rgba(47, 132, 71, 0.8)',
+      highlightFill: 'rgba(47, 132, 71, 0.8)',
+      highlightStroke: 'rgba(47, 132, 71, 0.8)'
+    }
+  ];
+  chartOptions = {
+    responsive: true
+  };
+  chartData = [{ data: [], label: 'CPU' }];
+  chartLabels = [];
+
   ngOnInit() {
-    //
+    let date = new Date();
+    date.setSeconds(date.getSeconds() - 5);
+    this.pushLabel(date);
+    this.chartData[0].data.push(Math.random() * 200 + 20);
+    date = new Date();
+    this.pushLabel(date);
+    this.chartData[0].data.push(Math.random() * 200 + 20);
+    interval(5000).subscribe(() => {
+      const arr = [...this.chartLabels];
+      if (arr.length === 10) {
+        arr.shift();
+        this.chartData[0].data.shift();
+      }
+      const date = new Date();
+      arr.push(`${date.getMinutes()}:${date.getSeconds()}`);
+      this.chartData[0].data.push(Math.random() * 200 + 20);
+      this.chartLabels = [...arr];
+    });
+  }
+
+  private pushLabel(date: Date) {
+    this.chartLabels.push(`${date.getMinutes()}:${date.getSeconds()}`);
   }
 
   ngOnChanges(changes: SimpleChanges) {
@@ -18,5 +54,9 @@ export class AppComponent implements OnChanges, OnInit {
       this.state = JSON.parse(changes['state'].currentValue);
       console.log(this.state);
     }
+  }
+
+  onChartClick(event) {
+    this.chartLabels.push('abc');
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule, Injector } from "@angular/core";
-import { createCustomElement } from "@angular/elements";
+import { Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { BrowserModule } from '@angular/platform-browser';
+import { Chart } from 'chart.js';
+import { ChartsModule } from 'ng2-charts';
+import { AppComponent } from './app.component';
 
-import { AppComponent } from "./app.component";
+const chart = Chart;
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, ChartsModule],
   entryComponents: [AppComponent]
 })
 export class AppModule {
   constructor(injector: Injector) {
-    customElements.define(
-      "dashboard-view",
-      createCustomElement(AppComponent, { injector })
-    );
+    customElements.define('dashboard-view', createCustomElement(AppComponent, { injector }));
   }
 
   ngDoBootstrap() {}
